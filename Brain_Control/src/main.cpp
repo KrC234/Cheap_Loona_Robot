6b5d74874c4c575709770e8f0d852ba4dc3df5a4
KrC234 <aalanruiz889@gmail.com>
2025-12-05 13:55:59 -0600

    Definición de Servos

--- Brain_Control/src/main.cpp
+++ Brain_Control/src/main.cpp
@@ -10,6 +10,9 @@
 #define RXD2 16
 #define TXD2 17
 
+Servo motorDer;
+Servo motorIzq;
+
 // Punto de Acceso
 const char *ssid = "ProyectoRobotica";
 const char *password = "123456789";
@@ -27,6 +30,12 @@ void setup() {
   // Configuración para arduino
   Serial2.begin(9600,SERIAL_8N1, RXD2, TXD2);
 
+  // Servos
+  motorDer.attach(18);
+  motorIzq.attach(19);
+
+  motorDer.write(0);
+  motorIzq.write(180);
   // Creación punto de acceso
   WiFi.disconnect(true);
   WiFi.mode(WIFI_AP);
@@ -38,4 +47,30 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
+
+  // Detecta si ha llegado un paquete
+  int packetSize = udp.parsePacket();
+  if(packetSize){
+    int len = udp.read(packetBuffer, 255);
+    
+    if(len > 0) packetBuffer[len] = 0;
+
+    char emocion = packetBuffer[0];
+
+    Serial2.write(emocion);
+    
+    // Casos de emocion para los motores
+    switch (emocion)
+    {
+    case '0':
+      /* code */
+      break;
+    case '1':
+      break;
+    case '2': 
+      break;
+    case '3':
+      break;
+    }
+  }
 }
